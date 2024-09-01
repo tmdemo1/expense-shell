39cd7833f1b83fc7e11e974c24832598e1700cc8
TRINADH <tmandalapu.blr@gmail.com>
2024-09-01 16:27:40 +0530

    mysql script

--- backend.sh
+++ backend.sh
@@ -69,5 +69,6 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading backend application code"
 
  cd /app
+ rm -rf /app/* &>>$LOG_FILE
  unzip /tmp/backend.zip | tee -a $LOG_FILE
  VALIDATE $? "Extracting backend application code"
