fdf9d31eaf65e7bdba68a3dab7ae5290efd67d86
TRINADH <tmandalapu.blr@gmail.com>
2024-09-01 16:24:27 +0530

    mysql script

--- backend.sh
+++ backend.sh
@@ -68,6 +68,6 @@ VALIDATE $? "Creating app folder"
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip  &>>$LOG_FILE
 VALIDATE $? "Downloading backend application code"
 
-# cd /app
-# unzip /tmp/backend.zip  &>>$LOG_FILE
-# VALIDATE $? "Extracting backend application code"
+ cd /app
+ unzip /tmp/backend.zip &>>$LOG_FILE
+ VALIDATE $? "Extracting backend application code"
